628136b0b7976e3e7167d839aad0f3c578ddf03c
David Walker <dave@sitecoredave.com>
2023-02-06 21:56:10 -0600

    save images

--- _data/amazon.js
+++ _data/amazon.js
@@ -1,5 +1,7 @@
 const fetch = require("node-fetch");
+const fs = require("fs");
 const cheerio = require("cheerio");
+const request = require('request');
 module.exports = async function () {
   const url = 'https://www.amazon.com/s?i=stripbooks&rh=p_27%3ABlessed+Beyond+Foundation&s=review-count-rank&qid=1675694725&text=Blessed+Beyond+Foundation&ref=sr_st_review-count-rank&ds=v1%3Af3NR3vKuhKOapwA0%2BCiPnpgkm5O0dGNcopFxUZlwUGo';
   const books = [];
@@ -7,9 +9,9 @@ module.exports = async function () {
     const response = await fetch(url);
     const html = await response.text();
     const $ = cheerio.load(html);
-    console.log('************************************************************************');
-    console.log('parsing books');
-    console.log('************************************************************************');
+    //console.log('************************************************************************');
+   // console.log('parsing books');
+    //console.log('************************************************************************');
     $('div.s-result-item').each((index, bookElement) => {
         //console.log(bookElement);
       const titleElement = $(bookElement).find('a.a-link-normal');
@@ -35,10 +37,18 @@ module.exports = async function () {
             link = `https://www.amazon.com${link}`;
         }
         const imageElement = $(bookElement).find('img.s-image');
-        const image = imageElement.length ? imageElement.attr('src') : '';
+        //const image = imageElement.length ? imageElement.attr('src') : '';
         const format = 'Paperback';
         const price = $(bookElement).find('span.a-offscreen').text();
-        console.log('price:' + price);
+        //console.log('price:' + price);
+
+        const imageUrl = imageElement.length ? imageElement.attr('src') : '';
+        const image = `./_assets/images/${title.replace(/\s+/g, '-').toLowerCase()}.png`;
+
+        request(imageUrl).pipe(fs.createWriteStream(image)).on('close', () => {
+          console.log(`Image saved: ${image}`);
+        });
+
         books.push({ description, format, image, link, price, series, subtitle, tags: ["books", "coloring-books", "products"], title });
       }
     });
